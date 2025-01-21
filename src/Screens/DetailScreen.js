a60a8e2bbd74ec21fd1c7b2b926f9f611f7b25fb
AndreaConrado <conrado.andrea@gmail.com>
2025-01-21 16:15:29 +0100

    API implementation for product details with display of image, title, description and price

--- src/Screens/DetailScreen.js
+++ src/Screens/DetailScreen.js
@@ -1,13 +1,39 @@
-import { View, Text } from 'react-native';
+import {Text, Image, StyleSheet, ActivityIndicator, ScrollView, TouchableOpacity, View } from 'react-native';
 import { useRoute } from '@react-navigation/native';
+import { useState,useEffect } from 'react';
 
 export default function DetailScreen() {
   const route = useRoute();
   const { id } = route.params || {};
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProductDetails = async () => {
+      try {
+        const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+        const data = await response.json();
+        setProduct(data);
+        setLoading(false)
+      } catch (error) {
+        console.error(error);
+        setLoading(false)
+      }
+    };
+
+    fetchProductDetails();
+  }, [id]);
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="rgba(76, 168, 133, 0.88)" />;
+  }
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      {id ? <Text>{`ID ricevuto: ${id}`}</Text> : <Text>Nessun parametro ricevuto</Text>}
+    <View>
+        <Image source={{uri: product.image}} style={{width: '100%',height: 500,resizeMode: 'contain',}}/>
+        <Text>{product.title}</Text>
+        <Text>{product.description}</Text>
+        <Text>${product.price}</Text>
     </View>
   );
 }
